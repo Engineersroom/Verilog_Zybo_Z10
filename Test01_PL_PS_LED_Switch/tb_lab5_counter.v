8b2f27edff1612d014c9ba14cae8190e9ee73c70
Engineersroom <eunsang284@gmail.com>
2023-03-15 17:21:50 +0900

    일단완료

--- Test01_PL_PS_LED_Switch/tb_lab5_counter.v
+++ Test01_PL_PS_LED_Switch/tb_lab5_counter.v
@@ -1,11 +1,16 @@
 `timescale 1ns/1ps
 
-module lab5_count_tb();
+module tb_lab5_counter();
 
 reg clk;
 reg reset_n;
 reg [3:0] sw;
 wire [3:0] led;
+
+always
+    #5 clk = ~clk;
+
+integer i;
 
 lab5_count u_lab5_count
 (
@@ -16,36 +21,35 @@ lab5_count u_lab5_count
 );
 
 initial begin
-    clk = 0;
+//initialize value
+$display("initialize value [%0d]", $time);
+    reset_n = 1;
+    clk     = 0;
+	sw 	= 0;
+
+// reset_n gen
+$display("Reset! [%0d]", $time);
+# 100
     reset_n = 0;
-    sw = 4'b0000;
-    #10 reset_n = 1;
+# 10
+    reset_n = 1;
+# 10
+@(posedge clk);
 
-    repeat(20) begin
-        #5 clk = ~clk;
-    end
+$display("sw on [%0d]", $time);
+sw = 4'b1111;
 
-    sw = 4'b0001; // enable counter_toggle_out_25M
-    repeat(50) begin
-        #5 clk = ~clk;
-    end
+$display("Start! [%0d]", $time);
 
-    sw = 4'b0010; // enable counter_toggle_out_50M
-    repeat(50) begin
-        #5 clk = ~clk;
-    end
-
-    sw = 4'b0100; // enable counter_toggle_out_100M
-    repeat(50) begin
-        #5 clk = ~clk;
-    end
-
-    sw = 4'b1000; // enable counter_toggle_out_200M
-    repeat(50) begin
-        #5 clk = ~clk;
-    end
-
-    $finish;
+for (i = 0; i < 32'd400; i = i+1) begin
+	@(posedge clk);
 end
+
+# 100
+$display("Success Simulation!! (Matbi = gudok & joayo) [%0d]", $time);
+$finish;
+end
+
+
 
 endmodule
---
+++ Test01_PL_PS_LED_Switch/xsim.dir/tb_lab5_counter/xsim_script.tcl
@@ -0,0 +1,4 @@
+set_param project.enableReportConfiguration 0
+load_feature core
+current_fileset
+xsim {tb_lab5_counter} -wdb {simulate_xsim_tb_lab5_counter.wdb} -autoloadwcfg
